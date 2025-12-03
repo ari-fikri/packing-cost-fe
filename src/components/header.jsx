c9b70aad133fc035d271f4bfb2b697aa21f3c709
Ari Fikri <arirushan79@gmail.com>
2025-12-03 13:34:01 +0700

    Add Notification list in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -18,15 +18,44 @@ return (
 
 
 <ul className="navbar-nav ml-auto">
-<li className="nav-item">
-<a className="nav-link" data-widget="fullscreen" href="#" role="button"><i className="fas fa-expand-arrows-alt"></i></a>
-</li>
-{auth.user && (
-<li className="nav-item d-none d-sm-inline-block">
-<span className="nav-link">{auth.user.username}</span>
-</li>
-)}
-</ul>
+      {/* Notification Dropdown Menu */}
+      <li className="nav-item dropdown">
+        <a className="nav-link" data-toggle="dropdown" href="#">
+          <i className="far fa-bell"></i>
+          <span className="badge badge-warning navbar-badge">2</span>
+        </a>
+        <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span className="dropdown-item dropdown-header">2 Notifications</span>
+          <div className="dropdown-divider"></div>
+          <div className="dropdown-item">
+            <p style={{fontWeight: 'bold'}}>Date : 20 Feb 2025 13:15</p>
+            <p className="text-sm">You have new form request to review.</p>
+            <p className="text-sm text-muted">From : Jonathan Hairi</p>
+            <p className="text-sm text-muted">Dept : PPMD</p>
+            <button className="btn btn-primary btn-sm">See Detail</button>
+          </div>
+          <div className="dropdown-divider"></div>
+          <div className="dropdown-item">
+            <p style={{fontWeight: 'bold'}}>Date : 19 Feb 2025 12:15</p>
+            <p className="text-sm">Your form submission has been rejected</p>
+            <p className="text-sm text-muted">From : Jonathan Hairi</p>
+            <p className="text-sm text-muted">Dept : PPMD</p>
+            <p className="text-sm text-muted">Comment: Sheet tersebut sudah tidak ada di pasaran ...</p>
+            <button className="btn btn-primary btn-sm">See Detail</button>
+          </div>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" data-widget="fullscreen" href="#" role="button"><i className="fas fa-expand-arrows-alt"></i></a>
+      </li>
+      {auth.user && (
+        <li className="nav-item d-none d-sm-inline-block">
+          <span className="nav-link">{auth.user.username}</span>
+        </li>
+      )}
+    </ul>
 </nav>
 )
 }
