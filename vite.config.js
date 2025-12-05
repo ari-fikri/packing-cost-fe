738c29d75db2d1fa454c5cd190e07e8c7b371cbf
Ari Fikri <arirushan79@gmail.com>
2025-12-05 16:14:53 +0700

    add  ghPages plugin for 404 redirection

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: command === "build" ? "/packing-cost-fe/" : "/",
   build: {
     rollupOptions: {
